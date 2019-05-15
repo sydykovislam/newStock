1aa7d9b49f2aca28d5fb7e2504fd422af28fd9c9
Sydykov Islam <iskasydykov@gmail.com>
2019-05-15 15:43:16 +0600

    Add examples

--- src/components/FullArticle/FullArticle.js
+++ src/components/FullArticle/FullArticle.js
@@ -6,8 +6,12 @@ import classes from "./FullArticle.module.css";
 function FullArticle(props) {
   // console.log(props.flArt);
   return (
-    <div className="FullArticle">
+    <div className={classes.FullArticle}>
       <h1 className={classes.Title}>{props.flArt.title}</h1>
+      <img src={props.flArt.preview} />
+      <div className={classes.text}>
+        <p>{props.flArt.content}</p>
+      </div>
     </div>
   );
 }
