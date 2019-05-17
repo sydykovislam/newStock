31f99016a529d6a844fdbdd7aa83b527be2d9924
Sydykov Islam <iskasydykov@gmail.com>
2019-05-17 11:43:15 +0600

    Try to add add new Articles logic

--- src/pages/AdminPanel/AticlesPanel/ArticlesPanel.js
+++ src/pages/AdminPanel/AticlesPanel/ArticlesPanel.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import What from './components/What/What';
 import classes from "./ArticlesPanel.module.css";
+import axios from "../../../axios";
 
 class ArticlesPanel extends Component {
   newArticlePrw = {
@@ -16,36 +17,71 @@ class ArticlesPanel extends Component {
     preview: "url"
   };
 
-  setTitle = () => {};
-  setContent = () => {};
-  setContent = () => {};
+  // let createFunc = (com) => {
+  //   let newCom = event.target.value;
+
+  //   newArticlePrw.{com} = newCom;
+
+  // }
+  setTitle = event => {
+    let newCom = event.target.value;
+    this.newArticlePrw.title = newCom;
+    this.newArticle.title = newCom;
+  };
+  setContentPrw = event => {
+    let newCom = event.target.value;
+    this.newArticlePrw.content = newCom;
+  };
+  setContent = event => {
+    let newCom = event.target.value;
+    this.newArticle.content = newCom;
+  };
+  setUrl = event => {
+    let newCom = event.target.value;
+    this.newArticlePrw.preview = newCom;
+    this.newArticle.preview = newCom;
+  };
+
+  addArticle = () => {
+    let id = Math.random() * 10000;
+    let newArtclPrw = { ...this.newArticlePrw };
+    newArtclPrw.id = id.toFixed(0);
+    let article = newArtclPrw;
+
+    axios.post("articles.json", article).then(response => {
+      alert("Success");
+    });
+  };
+
   render() {
-    // let createFunc = (com) => {
-    //   let newCom = event.target.value;
-
-    //   newArticlePrw. = newCom;
-
-    // }
     return (
       <div className={classes.ArticlesPanel}>
         <h1>Add article</h1>
         <input
+          onChange={this.setTitle}
           className={classes.Inputs}
           type="text"
           placeholder="Article`s title"
         />
         <input
+          onChange={this.setUrl}
           className={classes.Inputs}
           type="text"
           placeholder="Picture`s url"
         />
         <input
+          onChange={this.setContentPrw}
           className={classes.Inputs}
           type="text"
-          placeholder="Preview text"
+          placeholder="Preview content"
         />
-        <input className={classes.Inputs} type="text" placeholder="Full text" />
-        <button>Add</button>
+        <input
+          onChange={this.setContent}
+          className={classes.Inputs}
+          type="text"
+          placeholder="Full content"
+        />
+        <button onClick={this.addArticle}>Add</button>
       </div>
     );
   }
