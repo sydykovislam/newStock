fbb46bf7ad9f16d6fc5324a9c739604b0106df67
Sydykov Islam <iskasydykov@gmail.com>
2019-05-30 22:09:40 +0600

    Add galleries components

---
+++ src/components/PsGalleryPrw/PsGalleryPrw.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import What from './What/What';
+import classes from "./PsGalleryPrw.module.css";
+
+function PsGalleryPrw(props) {
+  return <div className="PsGalleryPrw">asd</div>;
+}
+
+export default PsGalleryPrw;
--- src/pages/PhotosGallery/PhotosGallery.js
+++ src/pages/PhotosGallery/PhotosGallery.js
@@ -1,13 +1,45 @@
-import React from "react";
-// import What from "./What/What";
+import React, { Component } from "react";
+// import What from './components/What/What';
 import classes from "./PhotosGallery.module.css";
+import axios from "../../axios";
 
-function PhotosGallery(props) {
-  return (
-    <div className={classes.PhotosGallery}>
-      <h1>PS Gallery</h1>
-    </div>
-  );
+class PhotoGallery extends Component {
+  state = {
+    galleries: null
+  };
+
+  componentDidMount() {
+    axios.get("psGalleries.json").then(response => {
+      this.setState({
+        articles: response.data
+      });
+      this.props.loadData(response.data);
+    });
+  }
+  render() {
+    return (
+      <div className={classes.PhotosGallery}>
+        <h1>PS Galleries</h1>
+      </div>
+    );
+  }
 }
 
-export default PhotosGallery;
+const mapDispatchToProps = dispatch => {
+  return {
+    loadData: data => dispatch({ type: "LOAD_DATA", data })
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    // this.props.ingredients: reducer.js/state.ingredients
+    articles: state.articles
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  null
+)(PhotoGallery);
